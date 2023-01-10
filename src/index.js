d2e1a69b1d810db0b5e9c73cf64bf53908520724
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-11 00:15:15 +0300

    replace Calculator import with App import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calculator from './components/Calculator';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Calculator />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
