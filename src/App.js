0fc79e02ec5b405ae0bbd88afa98e719a15e2e10
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-11 00:14:05 +0300

    add APP file with App class

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Calculator from './components/Calculator';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
