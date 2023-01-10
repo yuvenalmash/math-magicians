0e025f8549049750d82eabd24491c112fcc9af28
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-10 18:53:10 +0300

    import and call calculator component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <p>Hello world!</p>,
+  <Calculator />,
 );
