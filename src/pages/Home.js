96e70e02dfcefd267e3209227b57d48b9be9eea7
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-18 18:47:59 +0300

    add pages

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import './css/Home.css';
+
+const Home = () => (
+  <section id="homeContainer">
+    <h1>Welcome to our page!</h1>
+    <p className="homeP">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <p className="homeP">
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+      ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
+      tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
+      Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+    </p>
+  </section>
+);
+
+export default Home;
---
+++ src/pages/NoPage.js
@@ -0,0 +1,3 @@
+const NoPage = () => <h1>404</h1>;
+
+export default NoPage;
